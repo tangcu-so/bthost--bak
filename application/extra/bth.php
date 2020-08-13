bd6aa14593ffd2ec63658704b793bb0f5836a70e
1170535111@qq.com <1170535111@qq.com>
2020-08-13 09:55:54 +0800

    框架主体同步

---
+++ application/extra/bth.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    // 项目名
+    'APP_NAME'              => 'bthost',
+    // 版本号
+    'version'               => '1.0.0.20200813_beta',
+    // API接口地址
+    'api_url'               => 'http://auths.yum6.cn',
+    // 自动检测更新
+    'checkupdate'           => true,
+    // 安全密钥
+    'AUTH_KEY'              => 'djltSB5hH6v7op8IL2',
+    // 登录有效期
+    'COOKIE_EXPIRE'         => '86400',
+    // 自动获取公告
+    'getnotice'             => true,
+];
