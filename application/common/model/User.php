1601b5602458088b6ace2955af898b39c7a12748
1170535111@qq.com <1170535111@qq.com>
2020-10-05 14:13:46 +0800

    软删除model

--- application/common/model/User.php
+++ application/common/model/User.php
@@ -3,6 +3,7 @@
 namespace app\common\model;
 
 use think\Model;
+use traits\model\SoftDelete;
 
 /**
  * 会员模型
@@ -10,11 +11,14 @@ use think\Model;
 class User extends Model
 {
 
+    use SoftDelete;
+
     // 开启自动写入时间戳字段
     protected $autoWriteTimestamp = 'int';
     // 定义时间戳字段名
     protected $createTime = 'createtime';
     protected $updateTime = 'updatetime';
+    protected $deleteTime = 'deletetime';
     // 追加属性
     protected $append = [
         // 'url',
