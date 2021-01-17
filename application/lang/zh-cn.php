9314f247782146c68a180e56a4b79350741501e3
1170535111@qq.com <1170535111@qq.com>
2021-01-17 21:35:33 +0800

    优化授权提示

--- application/lang/zh-cn.php
+++ application/lang/zh-cn.php
@@ -5,7 +5,7 @@ return [
     'The current server public network IP acquisition failed, please make sure that your panel has public network capability, and check whether the server communication and key are correct'   => '当前服务器公网IP获取失败，请确保你的面板有公网能力，并检查服务器通讯及密钥是否正确',
     'Authorization check failed'                                                                            => '授权检查失败',
     'security_code error'                                                                                   => '授权码错误，请删除或修改文件/application/extra/auth.php文件中的授权码',
-    'Authorization information error, please request authorization again or obtain authorization code'      => '授权信息错误，请重新请求授权或获取授权码',
+    'Authorization information error, please request authorization again or obtain authorization code'      => '授权信息错误，请删除或修改文件/application/extra/auth.php文件中的授权码',
     'security_code failed'                                                                                  => '授权码不正确',
     'Authorization expired'                                                                                 => '授权已过期',
     'IP'                                                                                                    => '授权IP',
