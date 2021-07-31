8e6a356d372dbe40774ec0ad5092726a3a746dce
1170535111@qq.com <1170535111@qq.com>
2021-07-31 11:03:19 +0800

    修复子目录绑定高危功能模块

--- application/extra/bty.php
+++ application/extra/bty.php
@@ -12,15 +12,15 @@ return [
     // 项目名
     'APP_NAME'        => 'bthost',
     // 应用版本
-    'app_version'     => '1.6.0',
+    'app_version'     => '1.6.1',
     // 发布时间
-    'release_time'    => '20210711',
+    'release_time'    => '20210731',
     // 修订版本
     'revise_version'  => '0',
     // 测试版
     'is_beta'         => '0',
     // 完整版本号
-    'version'         => '1.6.0+20210711',
+    'version'         => '1.6.1+20210731',
     // API接口地址
     'api_url'         => 'https://auths.yum6.cn',
     'api_url2'        => 'https://auths.apip.wang',
