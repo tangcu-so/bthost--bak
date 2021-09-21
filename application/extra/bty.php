91119b00e09503b79051fbda3e9230f7fa39a662
1170535111@qq.com <1170535111@qq.com>
2021-09-21 12:28:43 +0800

    版本更新

--- application/extra/bty.php
+++ application/extra/bty.php
@@ -12,15 +12,15 @@ return [
     // 项目名
     'APP_NAME'        => 'bthost',
     // 应用版本
-    'app_version'     => '1.6.1',
+    'app_version'     => '1.6.2',
     // 发布时间
-    'release_time'    => '20210731',
+    'release_time'    => '20210921',
     // 修订版本
     'revise_version'  => '0',
     // 测试版
     'is_beta'         => '0',
     // 完整版本号
-    'version'         => '1.6.1+20210731',
+    'version'         => '1.6.2+20210921',
     // API接口地址
     'api_url'         => 'https://auths.yum6.cn',
     'api_url2'        => 'https://auths.apip.wang',
